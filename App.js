bb07375c67866a9c3b4f22263f48598de6b114b1
Kazi Anis Ibne Wali <98194481+Ojogor@users.noreply.github.com>
2023-05-19 10:21:15 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+export default function App() {
+            // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            {cards}
+        </div>
+    )
+}
+
